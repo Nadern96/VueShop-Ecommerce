4df928b3efa7025041aa0d066b8db8dbbaa58718
Nader Nabil <nadernabilmansour@gmail.com>
2020-06-12 12:58:42 +0200

    Add - products delete-edit data function

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -41,23 +41,98 @@
         </div>
         <hr />
 
-        <h3>product list</h3>
+        <h3>Products list</h3>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-            </tr>
-          </thead>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Modify</th>
+              </tr>
+            </thead>
 
-          <tbody>
-            <tr v-for="product in products" :key="product.id">
-              <td>{{ product.name }}</td>
-              <td>{{ product.price }}</td>
-            </tr>
-          </tbody>
-        </table>
+            <tbody>
+              <tr v-for="product in products" :key="product.id">
+                <td>{{ product.data().name }}</td>
+                <td>{{ product.data().price }}</td>
+                <td>
+                  <button @click="editProduct(product)" class="btn btn-primary">
+                    Edit
+                  </button>
+                  <button
+                    @click="deleteProduct(product.id)"
+                    class="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="editModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="editModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="editModalLabel">Edit Product</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <input
+                type="text"
+                placeholder="Product Name"
+                v-model="product.name"
+                class="form-control"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="text"
+                placeholder="Price"
+                v-model="product.price"
+                class="form-control"
+              />
+            </div>
+            <hr />
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              @click="updateProduct()"
+              type="button"
+              class="btn btn-primary"
+            >
+              Save changes
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -65,6 +140,7 @@
 
 <script>
 import { db } from "../firebase";
+import $ from "jquery";
 
 export default {
   name: "Products",
@@ -76,17 +152,50 @@ export default {
         name: null,
         price: null,
       },
+      active_item: null,
     };
   },
   methods: {
+    updateProduct() {
+      db.collection("products")
+        .doc(this.active_item)
+        .update(this.product)
+        .then(function() {
+          $("#editModal").modal("hide");
+          console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+          // The document probably doesn't exist.
+          console.error("Error updating document: ", error);
+        });
+    },
+    editProduct(product) {
+      $("#editModal").modal("show");
+      this.product = product.data();
+      this.active_item = product.id;
+    },
+    deleteProduct(docID) {
+      if (confirm("Are you sure ?")) {
+        db.collection("products")
+          .doc(docID)
+          .delete()
+          .then(function() {
+            console.log("Document successfully deleted!");
+          })
+          .catch(function(error) {
+            console.error("Error removing document: ", error);
+          });
+      }
+    },
     readData() {
+      this.reset();
       db.collection("products")
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
             console.log(doc.id, " => ", doc.data());
-            this.products.push(doc.data());
+            this.products.push(doc);
           });
         });
     },
@@ -111,6 +220,4 @@ export default {
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss"></style>
